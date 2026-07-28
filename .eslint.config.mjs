cd156fe6768c4c6faf21e12f2046cff654e8a13a
xLinorx <development@online.de>
2026-07-28 09:47:46 +0000

    Add new file

---
+++ .eslint.config.mjs
@@ -0,0 +1,19 @@
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ns: "readonly"
+            }
+        },
+        rules: {
+            "no-unused-vars": ["warn", { args: "none" }],
+            "no-undef": "error",
+            eqeqeq: "warn",
+            "no-console": "off",
+            "no-constant-condition": ["error", { checkLoops: false }]
+        }
+    }
+];

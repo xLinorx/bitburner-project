5bbfd17caaca458d35c4ef1ec3094f23cd58b836
xLinorx <development@online.de>
2026-07-28 10:31:51 +0000

    Edit eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,20 @@
 export default [
-    {
-        files: ["**/*.js"],
-        languageOptions: {
-            ecmaVersion: 2022,
-            sourceType: "module",
-            globals: {
-                ns: "readonly"
-            }
-        },
-        rules: {
-            "no-unused-vars": ["warn", { args: "none" }],
-            "no-undef": "error",
-            eqeqeq: "warn",
-            "no-console": "off",
-            "no-constant-condition": ["error", { checkLoops: false }]
-        }
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ns: "readonly",
+        console: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { args: "none" }],
+      "no-undef": "error",
+      eqeqeq: "warn",
+      "no-console": "off",
+      "no-constant-condition": ["error", { checkLoops: false }]
     }
+  }
 ];
